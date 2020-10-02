95715781a8d28fe67256ee742fc01a751a7fd012
nenekelvin <karvinmitchar@gmail.com>
2020-10-02 15:59:55 +0300

    define x and y axis

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,21 @@
 Le export default {
   name: 'HelloWorld',
   data: () => ({
-    chartOptionsBar: {}
+    chartOptionsBar: {chartOptionsBar: {
+  xAxis: {
+    data: ['Q1', 'Q2', 'Q3', 'Q4']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      type: 'bar',
+      data: [63, 75, 24, 92]
+    }
+  ]
+}
+}
   })
 }
 </script>
